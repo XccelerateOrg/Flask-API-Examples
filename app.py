8568c09c247b308ab12c6171c0f10d9a026cf762
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-05-28 17:26:21 +0800

    Merged the Flask together with the app.py for educational purpose.

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask ,jsonify ,request
+from flask import Flask ,jsonify ,request,render_template, Response
 
 app = Flask(__name__)
+import os
+from synthesizer import Synthesizer
 
-@app.route("/predict",methods=["POST"])
-def predict():
-    print(request.form.get('test'))
-    return jsonify({"abc":1234})
+
+
+@app.route("/",methods=["GET"])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/synthesize",methods=["GET"])
+def synthesize():
+  if not request.args.get('text'):
+    return {},400
+  return Response(
+    synthesizer.synthesize(request.args.get('text')),
+    headers={
+      'content_type' : 'audio/wav'
+    }
+  )
+
+
+synthesizer = Synthesizer()
+filedir = os.path.dirname(__file__)
+checkpoint = os.path.join(filedir,'model_checkpoint/model.ckpt')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+synthesizer.load(checkpoint)
+
+if __name__ == '__main__':
+  Flask.run(app)
+
